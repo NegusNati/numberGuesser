5cedf71e01b0b9ac3c602830bf9ba1c74c002d25
NegusNati <negusnati@gmail.com>
2023-01-19 04:11:07 -0800

    loss case

--- app.js
+++ app.js
@@ -28,15 +28,55 @@ UiGuessBtn.addEventListener("click", function () {
   }
 
   if (guess === winnningNum) {
-    // dispable the input
-    UiGuessInput.disabled = true;
-    //change the border
-    UiGuessInput.style.borderColor = "green";
-    //let them know they won
-    setMessage(`Your Guess Won the Game`, "green");
+    // // disable the input
+    // UiGuessInput.disabled = true;
+    // //change the border
+    // UiGuessInput.style.borderColor = "green";
+    // //let them know they won
+    // setMessage(`Your Guess Won the Game`, "green");
+
+    gameOver(true, `Your ${winnningNum} Guess Won the Game`);
   } else {
+    //wnat to subtract from guesses left
+    guessesLeft -= 1;
+
+    // if any guesses left
+    if (guessesLeft === 0) {
+      // game over - lost
+      // UiGuessInput.disabled = true;
+      // UiGuessInput.style.borderColor = "red";
+      // setMessage(
+      //   `You run out of trials, the correct number was ${winnningNum}. Game Over!`,
+      //   "red"
+      // );
+      gameOver(
+        false,
+        `You run out of trials, the correct number was ${winnningNum}. Game Over!`
+      );
+    } else {
+      // game resume - answer wrong
+      setMessage(
+        `${guess} is Incorrect, you have ${guessesLeft} guesses left`,
+        "red"
+      );
+      //clear the input
+      UiGuessInput.value = "";
+      UiGuessInput.style.borderColor = "Red";
+    }
   }
 });
+
+function gameOver(won, msg) {
+  let color;
+  won === true ? (color = "green") : (color = "red");
+
+  // disable the input
+  UiGuessInput.disabled = true;
+  //change the border
+  UiGuessInput.style.borderColor = color;
+  //let them know they won
+  setMessage(msg, color);
+}
 
 //set message
 
